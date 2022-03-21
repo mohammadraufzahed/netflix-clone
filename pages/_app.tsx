fb2c6061e745dbdd08e2f2e39f8be494776aac87
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-03-21 02:43:05 -0700

    Added the initile struct and added the home header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,48 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "normalize.css/normalize.css";
+import "styles/theme/index.scss";
+import "styles/fonts/index.scss";
+import "styles/global.scss";
+import { useEffect, useState } from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Custom AppProps Type with layout support
+type CustomAppProps = AppProps & {
+  Component: AppProps["Component"] & {
+    PageLayout?: React.ComponentType;
+  };
+};
+
+function MyApp({ Component, pageProps }: CustomAppProps) {
+  const [lowWidth, setLowWidth] = useState(false);
+  useEffect(() => {
+    window.onresize = () => {
+      if (window) {
+        if (window.outerWidth < 320) {
+          setLowWidth(true);
+        } else if (lowWidth) {
+          setLowWidth(false);
+        }
+      }
+    };
+  }, [lowWidth]);
+  if (lowWidth) {
+    return (
+      <div>
+        <h1>Your device width is to low.</h1>
+      </div>
+    );
+  }
+  return (
+    <>
+      {Component.PageLayout ? (
+        <Component.PageLayout>
+          <Component {...pageProps} />
+        </Component.PageLayout>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
